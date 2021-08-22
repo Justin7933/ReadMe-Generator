293f3f5f659c7f2180ef0044f085106fcbdcc7fa
Justin Damon <justindamon84@gmail.com>
2021-08-22 16:14:03 -0400

    additions

---
+++ index.js
@@ -0,0 +1,73 @@
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+const writeFileAsync = util.promisify(fs.writeFile);
+
+//Prompt the user questions to populate the README.md
+function promptUser(){
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "projectTitle",
+            message: "What is the project title?",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Write a brief description of your project: "
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "How did you install this: ",
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Chose the appropriate license for this project: ",
+            choices: [
+                "ISC",
+                "MIT",
+            ]
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "Who are the contributors of this projects?"
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "Is there a test included?"
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "Please enter your GitHub username: "
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email: "
+        }
+    ]);
+} 
+
+// Async function using util.promisify 
+  async function init() {
+    try {
+        // Ask user questions and generate responses
+        const data = await promptUser();
+        const generateContent = generateMarkdown(data);
+       
+        await writeFileAsync('.README.md', generateContent);
+        console.log('✔️  Successfully wrote to README.md');
+    }   catch(err) {
+        console.log(err);
+    }
+  }
+  
+  init();  
+
+
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,39 @@
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+    return `# ${data.title}
+  ## Description
+  🔍 ${data.description}
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  ## Installation
+  💾 ${data.installation}
+  ## Usage
+  💻 ${data.usage}
+  ## License
+  ![badge](https://img.shields.io/badge/license-${data.license}-brightgreen)
+  <br />
+  This application is covered by the ${data.license} license. 
+  ## Contributing
+  👪 ${data.contributing}
+  ## Tests
+  ✏️ ${data.tests}
+  ## Questions
+  ✋ ${data.questions}<br />
+  <br />
+  :octocat: Find me on GitHub: [${data.username}](https://github.com/${data.username})<br />
+  <br />
+  ✉️ Email me with any questions: ${data.email}<br /><br />
+  
+  `;
+  }
+  
+  
+  
+  module.exports = generateMarkdown;
+  
